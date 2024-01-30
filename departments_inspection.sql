ecb88df7ddd1a44b1808b75021f9922d71bf458b
Daniel Ng <117688408+Daniel-LeTC@users.noreply.github.com>
2024-01-30 22:44:21 +0700

    change department_id to int && find missing

---
+++ departments_inspection.sql
@@ -0,0 +1,19 @@
+-- View the first few rows from each table
+SELECT TOP 10 * FROM departments;
+
+-- Row counts
+SELECT COUNT(*) AS TotalRows 
+FROM departments;
+
+-- Table structures
+EXEC sp_help 'departments';
+
+-- Change datatype of department_id column from varchar to int
+ALTER TABLE departments 
+ALTER COLUMN department_id INT;
+
+-- Find missing values
+SELECT * 
+FROM departments 
+WHERE department_id IS NULL OR department IS NULL;
+
