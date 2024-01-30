81ea99001d9e227954a01b44e99be733d3a8125c
Daniel Ng <117688408+Daniel-LeTC@users.noreply.github.com>
2024-01-30 21:49:21 +0700

    data inspection for aisle table

---
+++ aisle_inspection.sql
@@ -0,0 +1,25 @@
+-- View the first few rows from each table
+SELECT TOP 5  *
+FROM aisles;
+
+-- Row counts
+SELECT COUNT(*) 
+FROM aisles;
+
+-- Table structures
+EXEC sp_help 'aisles';
+SELECT * 
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_NAME = 'aisles';
+
+-- Change datatype of aisle_id column from varchar to int
+ALTER TABLE aisles 
+ALTER COLUMN aisle_id INT;
+
+-- Find missing values
+SELECT * 
+FROM aisles 
+WHERE aisle IS NULL OR aisle like 'missing' OR aisle_id IS NULL;
+-- Update missing values
+
+-- Delete row with missing values
