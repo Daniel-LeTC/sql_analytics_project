0207a9b7dbcc0a8e65117ab4537ebf144564a9b6
danielwsl <117688408+Daniel-LeTC@users.noreply.github.com>
2024-02-01 13:49:44 +0700

    top product per user

---
+++ Analyzing_data.sql
@@ -0,0 +1,16 @@
+USE instacart_market
+-- Market Basket Analysis, show how these products often purchased with each other:
+SELECT op1.product_id AS Product1, op2.product_id AS Product2, COUNT(*) AS Frequency
+FROM order_products AS op1
+INNER JOIN order_products AS op2 
+    ON op1.order_id = op2.order_id 
+    AND op1.product_id < op2.product_id
+GROUP BY op1.product_id, op2.product_id
+ORDER BY Frequency DESC;
+
+-- Top products per user:
+SELECT o.user_id, op.product_id, COUNT(*) AS TotalOrders
+FROM orders AS o
+INNER JOIN order_products AS op ON o.order_id = op.order_id
+GROUP BY o.user_id, op.product_id
+ORDER BY o.user_id, TotalOrders DESC;
